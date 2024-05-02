87061f919d359479148037d962eefbc4d297c737
Frags <devcodedred@gmail.com>
2024-05-02 16:22:07 +0200

    Testing

---
+++ src/main/java/me/skyblockapi/SkyblockAPI.java
@@ -0,0 +1,17 @@
+package me.skyblockapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkyblockAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
