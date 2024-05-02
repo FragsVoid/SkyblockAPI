716a7b7424cf09dab390396213aa2a23db175dc4
Frags <adriwen600@gmail.com>
2024-05-02 16:41:11 +0200

    Testing

--- src/main/java/me/skyblockapi/SkyblockAPI.java
+++ src/main/java/me/skyblockapi/SkyblockAPI.java
@@ -1,17 +1,24 @@
 package me.skyblockapi;
 
+import me.skyblockapi.message.MessageManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkyblockAPI extends JavaPlugin {
 
+
+    public MessageManager messageManager;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public MessageManager getMessageManager() {
+        return messageManager;
     }
 }
