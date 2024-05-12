733562feebc5970581bd8f585c70ade9db15f322
Frags <adriwen600@gmail.com>
2024-05-12 11:13:00 +0200

    Testing

--- src/main/java/me/skyblockapi/SkyblockAPI.java
+++ src/main/java/me/skyblockapi/SkyblockAPI.java
@@ -1,19 +1,25 @@
 package me.skyblockapi;
 
 import me.skyblockapi.listeners.PortalListener;
+import me.skyblockapi.menu.PlayerMenuUtility;
 import me.skyblockapi.message.MessageManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.world.PortalCreateEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashMap;
 import java.util.function.Consumer;
 
 public final class SkyblockAPI extends JavaPlugin {
 
 
     public MessageManager messageManager;
+
+    private static final HashMap<Player, PlayerMenuUtility> playerMenuUtilityMap = new HashMap<>();
+
 
     @Override
     public void onEnable() {
@@ -28,6 +34,18 @@ public final class SkyblockAPI extends JavaPlugin {
 
     public void registerEvent() {
         getServer().getPluginManager().registerEvents(new PortalListener(), this);
+    }
+
+    public static PlayerMenuUtility getPlayerMenuUtilityMap(Player player) {
+        PlayerMenuUtility playerMenuUtility;
+        if (!(playerMenuUtilityMap.containsKey(player))) {
+            playerMenuUtility = new PlayerMenuUtility(player);
+            playerMenuUtilityMap.put(player, playerMenuUtility);
+
+            return playerMenuUtility;
+        } else {
+            return playerMenuUtilityMap.get(player);
+        }
     }
 
     public MessageManager getMessageManager() {
---
+++ src/main/java/me/skyblockapi/listeners/MenuListener.java
@@ -0,0 +1,26 @@
+package me.skyblockapi.listeners;
+
+import me.skyblockapi.menu.Menu;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.InventoryHolder;
+
+public class MenuListener implements Listener {
+
+    @EventHandler
+    public void onClick(InventoryClickEvent e) {
+        InventoryHolder holder = e.getInventory().getHolder();
+
+        if (holder instanceof Menu) {
+            e.setCancelled(true);
+            if (e.getCurrentItem() == null) {
+                return;
+            }
+
+            Menu menu = (Menu) holder;
+
+            menu.handleMenu(e);
+        }
+    }
+}
